94c2a63d0a169187b35f87a89734ca86380f4614
Dung <dung8421@gmail.com>
2021-06-11 08:39:14 +0700

    fix

--- mobile/android/app/src/main/kotlin/com/example/getx_pattern_ex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.getx_pattern_ex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile/lib/app/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-final String imageAssetsRoot = "images/";
-
-final String logo = _getImagePath("logo.png");
-
-String _getImagePath(String fileName) {
-  return imageAssetsRoot + fileName;
-}
